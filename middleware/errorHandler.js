8f86b945eb5767803a94a8c7c3ba13dc4677a036
EdgarDertycia <edgardertycia@hotmail.com>
2023-08-03 22:03:42 -0300

    first commit

---
+++ middleware/errorHandler.js
@@ -0,0 +1,16 @@
+// middleware/errorHandler.js
+const errorHandler = (err, req, res, next) => {
+    // Verificar si el error tiene un código de estado definido, de locontrario, establecer el codigo de estado predeterminado
+    const statusCode = err.statusCode || 500;
+    // Construir objeto de respuesta de error
+    const errorResponse = {
+    error: {
+    message: err.message || "Error interno del servidor",
+    code: err.code || "internal_error",
+    },
+    };
+    // Enviar respuesta de error en formato JSON
+    res.status(statusCode).json(errorResponse);
+    };
+    module.exports = errorHandler;
+    
